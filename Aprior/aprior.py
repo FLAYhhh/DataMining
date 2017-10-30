d2f0fdf743afdd96f1f8edc478a90c267958b55d
FLAYhhh <flayhhh@gmail.com>
2017-10-30 18:31:52 +0800

    reconstruct the code of aprior algorithm

--- Aprior/aprior.py
+++ Aprior/aprior.py
@@ -1,95 +1,101 @@
-support_rate = float(input('Input support rate : '))
-transaction_num = 0
-transactions = []
-item_dict = {} #key:item_tuple , value:count
-# compute the num of items
-with open('retail.dat') as f:
-    s = f.read().split()
-    l = list(map(int, s))
-    max = max(l)
-
-items_list = [0] * (max + 1)
-
-#creat a list of set: transactions (each set is a transaction)
-with open('retail.dat') as f:
-    line_list = f.read().splitlines()
-    for line in line_list:
-        transaction_num += 1
-        transactions.append( set(list((map(int,line.split())))) )
+import time
+def get_transactions(filename):
+    with open(filename) as f:
+        transaction_list = f.read().splitlines()
+        transactions = []
+        for transaction in transaction_list:
+            transactions.append(set(list(map(int, transaction.split()))))
+        return transactions
 
 
-print("transaction num: %d"%transaction_num)
-support_num = int(transaction_num*support_rate); print("support_num:%d" %(support_num))
+def get_L1(transactions: object, abs_min_support: object) -> object:
+    id_cont_dir = {}
+    for transaction in transactions:
+        for item_id in transaction:
+            if item_id in id_cont_dir:
+                id_cont_dir[item_id] += 1
+            else: id_cont_dir[item_id] = 1
 
-#compute the support num of each item
-for transac in transactions:
-    for item in transac:
-        items_list[item] += 1
+    L1_dir = {}
+    for id in id_cont_dir.keys():
+        if id_cont_dir[id] >= abs_min_support:
+            L1_dir[(id,)] = id_cont_dir[id]
 
-L = [[],[]]
-L_set = [[],[]]
-# compute L1
-for index in range(len(items_list)):
-    if items_list[index]>=support_num:
-        L[1].append([index])
+    return L1_dir
 
-#L_set.append(set(L[1]))
 
-for e in L[1]:
-    #print("item %d : %d" % (e[0] , items_list[e[0]]))
-    key = tuple(e)
-    item_dict[key] = items_list[e[0]]
+def aprior(transactions, L1_dir, min_sup):
+    itemset_cont_dir = L1_dir.copy()
+    L = [[]]
+    L1 = list(L1_dir.keys())
+    L1.sort()
+    L.append(L1)
+    k = 1
+    while L[k]!=[]:
+        L.append([])
+        for i in range(len(L[k])):
+            for j in range(i+1,len(L[k])):
+                if L[k][i][:-1] == L[k][j][:-1]:
+                    copy = list(L[k][i][:])      #L[k] is length-k frequent itemsets. form: list of set such as [(1,2),(3,5)]
+                    copy.append(L[k][j][-1])
+                    copy.sort()
+                    if not tuple(copy) in L[k+1]:
+                        L[k+1].append(tuple(copy))
 
-#print(L[1])
-#print(items_list)
-k = 1
-while L[k]!=[]:
-    L.append([])
-    for i in range(len(L[k])):
-        for j in range(i+1,len(L[k])):
-            if L[k][i][:-1] == L[k][j][:-1]:
-                copy = list(L[k][i][:])
-                copy.append(L[k][j][-1])
-                copy.sort()
-                L[k+1].append( tuple(copy) )
-    if k>1:
+        if k>1:
+            l_copy = L[k+1][:]
+            for itemset in l_copy:
+                for i in range(k+1):
+                    subset_list = list(itemset[:])
+                    subset_list.pop(i)
+                    subset_tuple = tuple(subset_list)
+                    if not subset_tuple in L[k]:
+                        if itemset in L[k+1]:
+                            L[k+1].remove(itemset)
+                        continue
+
+        for itemset in L[k+1]:
+            itemset_cont_dir[itemset] = 0;
+        for transaction in transactions:
+            for itemset in L[k+1]:
+                i_set = set(itemset)
+                if i_set <= transaction:
+                    itemset_cont_dir[itemset] += 1
+
+
         l_copy = L[k+1][:]
         for itemset in l_copy:
-            for i in range(k+1):
-                subset_list = list(itemset[:])
-                subset_list.pop(i)
-                subset_tuple = tuple(subset_list)
-                if not subset_tuple in L[k]:
-                    if itemset in L[k+1]:
-                        L[k+1].remove(itemset)
-                    continue
+            if itemset_cont_dir[itemset] < min_sup:
+                L[k+1].remove(itemset)
+                itemset_cont_dir.pop(itemset)
 
-    #generate frequent item set
-    # t_list=[]
-    # for itemset in L[k+1]:
-    #     t_list.append(tuple(itemset))
+        k += 1
+
+    return itemset_cont_dir
 
 
-    for items in L[k+1]:
-        item_dict[items] = 0
+def print_fre_itemsets(itemset_cont_dir):
+    key_list = list(itemset_cont_dir.keys())
+    for key in sorted(key_list,key = lambda x:(len(x),x) ):
+        print("items ",end='')
+        print(key,end='')
+        print(" :",end='')
+        print(itemset_cont_dir[key],end='')
+        print()
 
-    for transac in transactions:
-        for items in L[k+1]:
-            itemset = set(items)
-            if itemset <= transac:
-                item_dict[items] += 1
+def get_min_sup():
+    ans = float(input("Input related minimum support: "))
+    return ans
+if __name__ == '__main__':
+    start = time.clock()
 
-    l_copy = L[k+1][:] # in this for loop , t_list is changed , so it needs  a copy
-    for items in l_copy:
-        if item_dict[items] < support_num:
-            L[k+1].remove(items)
-            item_dict.pop(items)
-    k+=1
+    relate_min_sup = get_min_sup()
+    filename = "retail.dat"
+    transactions = get_transactions(filename)
+    absolut_min_sup = int(len(transactions)*relate_min_sup)
+    L1_dir = get_L1(transactions,absolut_min_sup)
+    itemset_cont_dir = aprior(transactions, L1_dir, absolut_min_sup)
+    print_fre_itemsets(itemset_cont_dir)
 
-key_list = list(item_dict.keys())
-for key in sorted(key_list,key = lambda x:(len(x),x) ):
-    print("items ",end='')
-    print(key,end='')
-    print(" :",end='')
-    print(item_dict[key],end='')
-    print()
+    elapsed = (time.clock() - start)
+    print("Time used:",elapsed)
